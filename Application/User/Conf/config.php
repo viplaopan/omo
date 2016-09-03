abd0d9e44a1532cd8902a4f96e87029e13658074
difeng pan <difeng pan>
2016-09-03 11:00:52 +0800

    Change db link info

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,6 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'qy8@xhX1(6T#"*>-,~.A2K[]n}l|<DsRYbctZ4e{'); //加密KEY
-define('UC_DB_DSN', 'mysql://homestead:secret@localhost:33060/ohmyoffice_db'); // 数据库连接，使用Model方式调用API必须配置此项
-//define('UC_DB_DSN', 'mysql://ohmyoffice:Pa1234560d31f219@47.90.4.173:3306/ohmyoffice_db'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://ohmyoffice:Pa1234560d31f219@47.90.4.173:3306/ohmyoffice_db'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'xh_'); // 数据表前缀，使用Model方式调用API必须配置此项
