435e00133d902047dd4f5d4e27e332be548e2a24
huangweihao <1105234332@qq.com>
2016-11-02 22:55:36 +0800

    weq

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
  * 项目正式部署后请设置为false
  */
 define('APP_DEBUG', true );
-
+//ssss
 /**
  * 应用目录设置
  * 安全期间，建议安装调试完成后移动到非WEB目录
